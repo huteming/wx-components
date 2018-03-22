676716652ebe23ff9c5a17f4ff356d17861fb22b
tmhu <tmhu@hua9group.com>
2018-03-22 20:07:29 +0800

    添加cell

--- components/cell/cell.js
+++ components/cell/cell.js
@@ -1,4 +1,3 @@
-// components/cell/cell.js
 Component({
   options: {
     multipleSlots: true // 在组件定义时的选项中启用多slot支持
@@ -17,7 +16,20 @@ Component({
     },
 
     label: {
-      type: String
+      type: String,
+      value: '',
+    },
+
+    labelWidth: {
+      type: Number,
+      value: '',
+      observer (val) {
+        if (val > 0) {
+          this.setData({
+            styleBody: `width: ${val}px;`
+          })
+        }
+      }
     },
 
     value: {
@@ -42,7 +54,11 @@ Component({
    * 组件的初始数据
    */
   data: {
+    styleHeader: '',
+    styleBody: '',
+    styleValue: '',
 
+    windowWidth: '',
   },
 
   /**
@@ -57,5 +73,53 @@ Component({
         })
       }
     },
+  },
+
+  attached (e) {
+    try {
+      const { windowWidth } = wx.getSystemInfoSync()
+      this.setData({ windowWidth })
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  ready () {
+    let width = this.data.windowWidth - 10 - 15
+    const { isLink, error } = this.data
+    // 减去 isLink 宽度
+    if (isLink) {
+      width -= 13
+    }
+    // 减去 error 宽度
+    if (error) {
+      width -= 30
+    }
+
+    let styleHeader = ''
+    let styleValue = ''
+    const query = this.createSelectorQuery()
+    query.select('#cell-header').boundingClientRect()
+    query.select('#cell-body').boundingClientRect()
+    query.exec(res => {
+      // 减去 icon 宽度
+      if (res[0] && res[0].width > 0) {
+        styleHeader = 'margin-right: 8px;'
+        width = width - res[0].width - 8
+      }
+      // 减去 label 宽度
+      width = width - res[1].width - 10
+      // 剩余 value 宽度
+      styleValue = `
+        width: ${width}px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      `
+      this.setData({
+        styleHeader,
+        styleValue,
+      })
+    })
   }
 })
